d92d7de57a864ce950680bba544a874dbcb6b8cd
Brad McNew <mcnewbrad@gmail.com>
2026-01-23 17:17:09 -0500

    feat(09-01): update main navigation structure for Axite
    - Remove Developers and Solutions dropdowns
    - Add Enterprise direct link
    - Add Trust direct link
    - Keep Product as only dropdown item
    - Remove unused imports

--- apps/www/data/nav.tsx
+++ apps/www/data/nav.tsx
@@ -1,10 +1,4 @@
-import { DevelopersDropdown } from 'components/Nav/DevelopersDropdown'
 import { ProductDropdown } from 'components/Nav/ProductDropdown'
-import { SolutionsDropdown } from 'components/Nav/SolutionsDropdown'
-
-import { data as DevelopersData } from 'data/Developers'
-import MainProductsData from 'data/MainProducts'
-import { navData as SolutionsData } from 'data/Solutions'
 
 export const getMenu = () => ({
   primaryNav: [
@@ -13,33 +7,22 @@ export const getMenu = () => ({
       hasDropdown: true,
       dropdown: <ProductDropdown />,
       dropdownContainerClassName: 'rounded-xl',
-      subMenu: MainProductsData,
-    },
-    {
-      title: 'Developers',
-      hasDropdown: true,
-      dropdown: <DevelopersDropdown />,
-      dropdownContainerClassName: 'rounded-xl',
-      subMenu: DevelopersData,
-    },
-    {
-      title: 'Solutions',
-      hasDropdown: true,
-      dropdown: <SolutionsDropdown />,
-      dropdownContainerClassName: 'rounded-xl',
-      subMenu: SolutionsData,
     },
     {
       title: 'Pricing',
       url: '/pricing',
     },
     {
-      title: 'Docs',
-      url: '/docs',
+      title: 'Enterprise',
+      url: '/enterprise',
     },
     {
       title: 'Blog',
       url: '/blog',
+    },
+    {
+      title: 'Trust',
+      url: '/trust',
     },
   ],
 })
