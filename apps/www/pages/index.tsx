63ca94d2c5b9beb8190e409318eb57df193291a6
Brad McNew <mcnewbrad@gmail.com>
2026-01-26 19:06:46 -0500

    feat(16-05): integrate Phase 16 sections into homepage
    - Add HowItWorksSection, SecuritySection, FinalCTASection
    - Remove Supabase sections: Products, HeroFrameworks, CustomerStories
    - Remove Supabase sections: BuiltWithSupabase, DashboardFeatures, TwitterSocialSection
    - Remove Supabase sections: OpenSourceSection, CTABanner
    - Remove unused getContent import
    - Final order: Hero -> ProofSection -> Logos -> TransformationSection -> PillarsSection -> HowItWorksSection -> SecuritySection -> FinalCTASection

--- apps/www/pages/index.tsx
+++ apps/www/pages/index.tsx
@@ -1,5 +1,4 @@
 import dynamic from 'next/dynamic'
-import getContent from '~/data/home/content'
 import Layout from '~/components/Layouts/Default'
 import Hero from '~/components/Hero/Hero'
 import Logos from '~/components/logos'
@@ -7,33 +6,21 @@ import Logos from '~/components/logos'
 const ProofSection = dynamic(() => import('~/components/ProofSection'))
 const TransformationSection = dynamic(() => import('~/components/TransformationSection'))
 const PillarsSection = dynamic(() => import('~/components/PillarsSection'))
-const Products = dynamic(() => import('~/components/Products/index'))
-const HeroFrameworks = dynamic(() => import('~/components/Hero/HeroFrameworks'))
-const CustomerStories = dynamic(() => import('components/CustomerStories'))
-const BuiltWithSupabase = dynamic(() => import('components/BuiltWithSupabase'))
-const DashboardFeatures = dynamic(() => import('~/components/DashboardFeatures'))
-const TwitterSocialSection = dynamic(() => import('~/components/TwitterSocialSection'))
-const OpenSourceSection = dynamic(() => import('~/components/OpenSourceSection'))
-const CTABanner = dynamic(() => import('components/CTABanner/index'))
+const HowItWorksSection = dynamic(() => import('~/components/HowItWorksSection'))
+const SecuritySection = dynamic(() => import('~/components/SecuritySection'))
+const FinalCTASection = dynamic(() => import('~/components/FinalCTASection'))
 
 const Index = () => {
-  const content = getContent()
-
   return (
     <Layout>
       <Hero />
       <ProofSection />
+      <Logos />
       <TransformationSection />
       <PillarsSection />
-      <Logos />
-      <Products {...content.productsSection} />
-      <HeroFrameworks />
-      <CustomerStories />
-      <BuiltWithSupabase />
-      <DashboardFeatures {...content.dashboardFeatures} />
-      <TwitterSocialSection {...content.twitterSocialSection} />
-      <OpenSourceSection />
-      <CTABanner className="border-none" />
+      <HowItWorksSection />
+      <SecuritySection />
+      <FinalCTASection />
     </Layout>
   )
 }
